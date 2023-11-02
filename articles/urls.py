650a9e01b102681f47a90d818e43617b4c31f894
bhj8286 <bjs219@naver.com>
2023-11-02 15:22:06 +0900

    Update기능 구현

--- articles/urls.py
+++ articles/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('<int:id>/', views.detail, name='detail'),
     path('new/', views.new, name='new'),
     path('create/', views.create, name='create'),
-    path('<int:id>/delete/', views.delete, name='delete')
+    path('<int:id>/delete/', views.delete, name='delete'),
+    path('<int:id>/edit/', views.edit, name='edit'),
+    path('<int:id>/update/', views.update, name='update')
 ]
